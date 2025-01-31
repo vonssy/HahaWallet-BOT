4296694d7489ba2a3d3941ed9fb6c49e55036865
vonssy <kak.reyvaldizakaria@gmail.com>
2025-01-31 17:04:12 +0700

    Update Loop Interval

--- bot.py
+++ bot.py
@@ -3,9 +3,9 @@ from aiohttp import (
     ClientSession,
     ClientTimeout
 )
-from colorama import *
-from datetime import datetime
 from fake_useragent import FakeUserAgent
+from datetime import datetime
+from colorama import *
 import asyncio, json, os, pytz
 
 wib = pytz.timezone('Asia/Jakarta')
@@ -63,12 +63,13 @@ class HahaWallet:
         except json.JSONDecodeError:
             return []
     
-    def hide_email(self, email):
-        local, domain = email.split('@', 1)
-        hide_local = local[:3] + '*' * 3 + local[-3:]
-        return f"{hide_local}@{domain}"
+    def mask_account(self, account):
+        if "@" in account:
+            local, domain = account.split('@', 1)
+            mask_account = local[:3] + '*' * 3 + local[-3:]
+            return f"{mask_account}@{domain}"
         
-    async def user_login(self, email: str, password: str, retries=3):
+    async def user_login(self, email: str, password: str, retries=5):
         url = "https://prod.haha.me/users/login"
         data = json.dumps({"email":email, "password":password})
         headers = {
@@ -88,7 +89,7 @@ class HahaWallet:
                 else:
                     return None
                 
-    async def user_login(self, email: str, password: str, retries=3):
+    async def user_login(self, email: str, password: str, retries=5):
         url = "https://prod.haha.me/users/login"
         data = json.dumps({"email":email, "password":password})
         headers = {
@@ -105,32 +106,12 @@ class HahaWallet:
                         return result['id_token']
             except (Exception, ClientResponseError) as e:
                 if attempt < retries - 1:
-                    await asyncio.sleep(2)
-                else:
-                    return None
+                    await asyncio.sleep(5)
+                    continue
                 
-    async def user_info(self, token: str, retries=3):
-        url = "https://prod.haha.me/wallet-api/graphql"
-        data = json.dumps({"operationName":None,"variables":{},"query":"{\n  getRankInfo {\n    rank\n    karma\n    karmaToNextRank\n    rankName\n    rankImage\n    __typename\n  }\n}"})
-        headers = {
-            **self.headers,
-            "Authorization": token,
-            "Content-Length": str(len(data))
-        }
-        for attempt in range(retries):
-            try:
-                async with ClientSession(timeout=ClientTimeout(total=30)) as session:
-                    async with session.post(url=url, headers=headers, data=data) as response:
-                        response.raise_for_status()
-                        result = await response.json()
-                        return result['data']['getRankInfo']
-            except (Exception, ClientResponseError) as e:
-                if attempt < retries - 1:
-                    await asyncio.sleep(2)
-                else:
-                    return None
+                return None
                 
-    async def user_balance(self, token: str, retries=3):
+    async def user_balance(self, token: str, retries=5):
         url = "https://prod.haha.me/wallet-api/graphql"
         data = json.dumps({"operationName":None,"variables":{},"query":"{\n  getKarmaPoints\n}"})
         headers = {
@@ -147,11 +128,12 @@ class HahaWallet:
                         return result['data']
             except (Exception, ClientResponseError) as e:
                 if attempt < retries - 1:
-                    await asyncio.sleep(2)
-                else:
-                    return None
+                    await asyncio.sleep(5)
+                    continue
                 
-    async def daily_checkin(self, token: str, retries=3):
+                return None
+                
+    async def daily_checkin(self, token: str, retries=5):
         url = "https://prod.haha.me/wallet-api/graphql"
         data = json.dumps({"operationName":None,"variables":{"timezone":"Asia/Jakarta"},"query":"query ($timezone: String) {\n  getDailyCheckIn(timezone: $timezone)\n}"})
         headers = {
@@ -168,11 +150,12 @@ class HahaWallet:
                         return result['data']
             except (Exception, ClientResponseError) as e:
                 if attempt < retries - 1:
-                    await asyncio.sleep(2)
-                else:
-                    return None
+                    await asyncio.sleep(5)
+                    continue
                 
-    async def claim_checkin(self, token: str, retries=3):
+                return None
+                
+    async def claim_checkin(self, token: str, retries=5):
         url = "https://prod.haha.me/wallet-api/graphql"
         data = json.dumps({"operationName":None,"variables":{"timezone":"Asia/Jakarta"},"query":"mutation ($timezone: String) {\n  setDailyCheckIn(timezone: $timezone)\n}"})
         headers = {
@@ -189,84 +172,70 @@ class HahaWallet:
                         return result['data']['setDailyCheckIn']
             except (Exception, ClientResponseError) as e:
                 if attempt < retries - 1:
-                    await asyncio.sleep(2)
-                else:
-                    return None
+                    await asyncio.sleep(5)
+                    continue
+                
+                return None
         
     async def process_accounts(self, email: str, password: str):
-        hide_email = self.hide_email(email)
-
         token = await self.user_login(email, password)
         if not token:
             self.log(
-                f"{Fore.MAGENTA+Style.BRIGHT}[ Account{Style.RESET_ALL}"
-                f"{Fore.WHITE+Style.BRIGHT} {hide_email} {Style.RESET_ALL}"
-                f"{Fore.RED+Style.BRIGHT}GET Token Failed{Style.RESET_ALL}"
-                f"{Fore.MAGENTA+Style.BRIGHT} ]{Style.RESET_ALL}"
+                f"{Fore.CYAN+Style.BRIGHT}Status  :{Style.RESET_ALL}"
+                f"{Fore.RED+Style.BRIGHT} Login Failed {Style.RESET_ALL}"
             )
             return
+        
+        self.log(
+            f"{Fore.CYAN+Style.BRIGHT}Status  :{Style.RESET_ALL}"
+            f"{Fore.GREEN+Style.BRIGHT} Login Success {Style.RESET_ALL}"
+        )
 
-        user = await self.user_info(token)
+        balance = "N/A"
+
+        user = await self.user_balance(token)
         if user:
-            self.log(
-                f"{Fore.MAGENTA+Style.BRIGHT}[ Account{Style.RESET_ALL}"
-                f"{Fore.WHITE+Style.BRIGHT} {hide_email} {Style.RESET_ALL}"
-                f"{Fore.MAGENTA+Style.BRIGHT}] [ Balance{Style.RESET_ALL}"
-                f"{Fore.WHITE+Style.BRIGHT} {user['karma']} Karma {Style.RESET_ALL}"
-                f"{Fore.MAGENTA+Style.BRIGHT}]{Style.RESET_ALL}"
-            )
-        else:
-            self.log(
-                f"{Fore.MAGENTA+Style.BRIGHT}[ Account{Style.RESET_ALL}"
-                f"{Fore.WHITE+Style.BRIGHT} {hide_email} {Style.RESET_ALL}"
-                f"{Fore.RED+Style.BRIGHT}Data Is None{Style.RESET_ALL}"
-                f"{Fore.MAGENTA+Style.BRIGHT} ]{Style.RESET_ALL}"
-            )
+            balance = user["getKarmaPoints"]
+            
+        self.log(
+            f"{Fore.CYAN+Style.BRIGHT}Balance :{Style.RESET_ALL}"
+            f"{Fore.WHITE+Style.BRIGHT} {balance} Karma {Style.RESET_ALL}"
+        )
         
         check_in = await self.daily_checkin(token)
-        if check_in:
-            is_claimable = check_in['getDailyCheckIn']
+        if not check_in:
+            self.log(
+                f"{Fore.CYAN+Style.BRIGHT}Check-In:{Style.RESET_ALL}"
+                f"{Fore.RED+Style.BRIGHT} GET Data Failed {Style.RESET_ALL}"
+            )
+            return
+        
+        is_claimable = check_in['getDailyCheckIn']
 
-            if is_claimable:
-                claim = await self.claim_checkin(token)
-                
-                if claim:
-                    points = "N/A"
-                    balance = await self.user_balance(token)
-                    if balance:
-                        points = balance['getKarmaPoints']
-
-                    self.log(
-                        f"{Fore.MAGENTA+Style.BRIGHT}[ Daily Check-In{Style.RESET_ALL}"
-                        f"{Fore.GREEN+Style.BRIGHT} Is Claimed {Style.RESET_ALL}"
-                        f"{Fore.MAGENTA+Style.BRIGHT}] [ Balance{Style.RESET_ALL}"
-                        f"{Fore.WHITE+Style.BRIGHT} {points} Karma {Style.RESET_ALL}"
-                        f"{Fore.MAGENTA+Style.BRIGHT}]{Style.RESET_ALL}"
-                    )
-                else:
-                    self.log(
-                        f"{Fore.MAGENTA+Style.BRIGHT}[ Daily Check-In{Style.RESET_ALL}"
-                        f"{Fore.RED+Style.BRIGHT} Isn't Claimed {Style.RESET_ALL}"
-                        f"{Fore.MAGENTA+Style.BRIGHT}]{Style.RESET_ALL}"
-                    )
+        if is_claimable:
+            claim = await self.claim_checkin(token)
+            
+            if claim:
+                self.log(
+                    f"{Fore.CYAN+Style.BRIGHT}Check-In:{Style.RESET_ALL}"
+                    f"{Fore.GREEN+Style.BRIGHT} Is Claimed {Style.RESET_ALL}"
+                )
             else:
                 self.log(
-                    f"{Fore.MAGENTA+Style.BRIGHT}[ Daily Check-In{Style.RESET_ALL}"
-                    f"{Fore.YELLOW+Style.BRIGHT} Is Already Claimed {Style.RESET_ALL}"
-                    f"{Fore.MAGENTA+Style.BRIGHT}]{Style.RESET_ALL}"
+                    f"{Fore.CYAN+Style.BRIGHT}Check-In:{Style.RESET_ALL}"
+                    f"{Fore.RED+Style.BRIGHT} Isn't Claimed {Style.RESET_ALL}"
                 )
         else:
             self.log(
-                f"{Fore.MAGENTA+Style.BRIGHT}[ Daily Check-In{Style.RESET_ALL}"
-                f"{Fore.RED+Style.BRIGHT} Data Is None {Style.RESET_ALL}"
-                f"{Fore.MAGENTA+Style.BRIGHT}]{Style.RESET_ALL}"
+                f"{Fore.CYAN+Style.BRIGHT}Check-In:{Style.RESET_ALL}"
+                f"{Fore.YELLOW+Style.BRIGHT} Is Already Claimed {Style.RESET_ALL}"
             )
     
     async def main(self):
         try:
             accounts = self.load_accounts()
             if not accounts:
-                self.log(f"{Fore.RED}No accounts loaded from 'accounts.json'.{Style.RESET_ALL}")
+                self.log(f"{Fore.RED}No Accounts Loaded.{Style.RESET_ALL}")
                 return
 
             while True:
@@ -276,18 +245,23 @@ class HahaWallet:
                     f"{Fore.GREEN + Style.BRIGHT}Account's Total: {Style.RESET_ALL}"
                     f"{Fore.WHITE + Style.BRIGHT}{len(accounts)}{Style.RESET_ALL}"
                 )
-                self.log(f"{Fore.CYAN + Style.BRIGHT}-{Style.RESET_ALL}"*75)
-            
+        
+                separator = "=" * 10
                 for account in accounts:
                     email = account.get('Email')
                     password = account.get('Password')
 
-                    if email and password:
+                    if "@" in email and password:
+                        self.log(
+                            f"{Fore.CYAN + Style.BRIGHT}{separator}[{Style.RESET_ALL}"
+                            f"{Fore.WHITE + Style.BRIGHT} {self.mask_account(email)} {Style.RESET_ALL}"
+                            f"{Fore.CYAN + Style.BRIGHT}]{separator}{Style.RESET_ALL}"
+                        )
                         await self.process_accounts(email, password)
-                        self.log(f"{Fore.CYAN + Style.BRIGHT}-{Style.RESET_ALL}"*75)
                         await asyncio.sleep(3)
 
-                seconds = 86400
+                self.log(f"{Fore.CYAN + Style.BRIGHT}={Style.RESET_ALL}"*43)
+                seconds = 12 * 60 * 60
                 while seconds > 0:
                     formatted_time = self.format_seconds(seconds)
                     print(
